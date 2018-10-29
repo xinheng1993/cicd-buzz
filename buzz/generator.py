0a310b7b61980cdb16f8055860e690a1cc2221eb
xinheng1993 <skartfreedom@msn.com>
2018-10-29 22:27:04 +0000

    change generator

--- buzz/generator.py
+++ buzz/generator.py
@@ -1,9 +1,9 @@
 from __future__ import print_function
 import random
 
-buzz = ('continuous testing', 'continuous integration',
+buzz = ('XIN HENG', 'continuous integration',
     'continuous deployment', 'continuous improvement', 'devops')
-adjectives = ('complete', 'modern', 'self-service', 'integrated', 'end-to-end')
+adjectives = ('A00971472', 'modern', 'self-service', 'integrated', 'end-to-end')
 adverbs = ('remarkably', 'enormously', 'substantially', 'significantly',
     'seriously')
 verbs = ('accelerates', 'improves', 'enhances', 'revamps', 'boosts')
